6a8e5c50fa6ea10dcdba58ab5325ecbfef435daa
dongilSeo <greatsun81@gmail.com>
2020-10-16 19:35:48 +0900

    변경됐어요

---
+++ hello.py
@@ -0,0 +1,10 @@
+# 변수, 자료형, 함수, 조건문, 반복문
+
+wizards = [
+    {'name': '해리', 'age': 40},
+    {'name': '허마이오니', 'age': 40},
+    {'name': '론', 'age': 41},
+]
+
+for wizard in wizards:
+    print(wizard['name'], wizard['age'])
